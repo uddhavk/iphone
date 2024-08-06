f483b58b45309c1efcfc945474012443c71478af
uddhavk <164346647+uddhavk@users.noreply.github.com>
2024-08-06 06:53:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,269 @@
+let abc = new Swiper(".mySwiper", {
+    slidesPerView: 7,
+    spaceBetween: 10,
+    loop:true,
+    autoplay:{
+        delay:1000,
+
+    },
+
+     keyboard:{
+        enabled:true,
+     },
+
+     mousewheel:true,
+
+     breakpoints:{
+        320:{
+            slidesPerView:8,
+        },
+
+        375:{
+            slidesPerView:8,
+  
+
+        },
+
+        425:{
+            slidesPerView:3,
+
+        },
+
+        768:{
+            slidesPerView:4,
+
+        },
+
+        1024:{
+            slidesPerView:8,
+
+        }
+     }
+
+})
+
+
+
+let ac = new Swiper(".mySwiper2", {
+    slidesPerView: 7,
+    spaceBetween: 20,
+    loop:true,
+    autoplay:{
+        delay:2500,
+
+    },
+
+     keyboard:{
+        enabled:true,
+     },
+
+     mousewheel:true,
+
+     breakpoints:{
+        320:{
+            slidesPerView:1,
+        },
+
+        375:{
+            slidesPerView:1,
+  
+
+        },
+
+        425:{
+            slidesPerView:1,
+
+        },
+
+        768:{
+            slidesPerView:2,
+
+        },
+
+        1024:{
+            slidesPerView:4,
+
+        }
+     }
+
+})
+
+
+
+
+let ab = new Swiper(".mySwiper3", {
+    slidesPerView: 7,
+    spaceBetween: 20,
+    loop:true,
+    // autoplay:{
+    //     delay:2000,
+
+    // },
+
+     keyboard:{
+        enabled:true,
+     },
+
+     mousewheel:true,
+
+     breakpoints:{
+        320:{
+            slidesPerView:1,
+        },
+
+        375:{
+            slidesPerView:1,
+  
+
+        },
+
+        425:{
+            slidesPerView:1,
+
+        },
+
+        768:{
+            slidesPerView:2,
+
+        },
+
+        1024:{
+            slidesPerView:3.5,
+
+        }
+     }
+
+})
+
+
+
+var swiper2 = new Swiper(".mySwiper4", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop:true,
+    autoplay:{
+        delay:2000,
+
+    },
+
+     keyboard:{
+        enabled:true,
+     },
+
+     mousewheel:true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+         375:{
+            slidesPerView:1,
+  
+
+        },
+
+        425:{
+            slidesPerView:1,
+
+        },
+
+        768:{
+            slidesPerView:2,
+
+        },
+
+        1024:{
+            slidesPerView:4.1,
+
+        }
+    }
+})
+
+
+
+
+var swiper2 = new Swiper(".mySwiper5", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop:true,
+    // autoplay:{
+    //     delay:1000,
+
+    // },
+
+     keyboard:{
+        enabled:true,
+     },
+
+     mousewheel:true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+         375:{
+            slidesPerView:1,
+  
+
+        },
+
+        425:{
+            slidesPerView:1,
+
+        },
+
+        768:{
+            slidesPerView:2,
+
+        },
+
+        1024:{
+            slidesPerView:4,
+
+        }
+    }
+})
+
+
+
+
+var swiper2 = new Swiper(".mySwiper6", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop:true,
+    // autoplay:{
+    //     delay:1000,
+
+    // },
+
+     keyboard:{
+        enabled:true,
+     },
+
+     mousewheel:true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+         375:{
+            slidesPerView:1,
+  
+
+        },
+
+        425:{
+            slidesPerView:1,
+
+        },
+
+        768:{
+            slidesPerView:2,
+
+        },
+
+        1024:{
+            slidesPerView:3.5,
+
+        }
+    }
+})
+
+
